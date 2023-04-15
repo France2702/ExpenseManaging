df7f4d554bd29736d1f9ad811b40484525d71876
CRQuater <rhquater@gmail.com>
2023-04-15 08:57:12 +0700

    Tinh giảm biên chế

--- app/src/main/java/com/example/expensemanaging/MainscreenActivity.java
+++ app/src/main/java/com/example/expensemanaging/MainscreenActivity.java
@@ -5,16 +5,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageButton;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainscreenActivity extends AppCompatActivity {
     FloatingActionButton btn_add_transaction;
+    ImageButton btn_home;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mainscreen);
         btn_add_transaction = (FloatingActionButton) findViewById(R.id.btn_add_transaction);
+        btn_home = findViewById(R.id.btn_home);
         btn_add_transaction.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -22,10 +25,22 @@ public class MainscreenActivity extends AppCompatActivity {
             }
         });
 
+        btn_home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                switchHomeActivities();
+            }
+        });
+
     }
 
     private void switchActivities() {
         Intent switchActivityIntent = new Intent(this, AddActive.class);
+        startActivity(switchActivityIntent);
+    }
+
+    private void switchHomeActivities() {
+        Intent switchActivityIntent = new Intent(this, MainscreenActivity.class);
         startActivity(switchActivityIntent);
     }
 }
